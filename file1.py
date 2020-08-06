3ec21a14dea8e08aa8059886b68a17a34dd72c93
sandeepsunny738 <32111038+sandeepsunny738@users.noreply.github.com>
2020-08-05 22:01:14 -0400

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,10 +1,10 @@
-# 1. Write a Python program to sum all the items in a list.
+## 1. Write a Python program to sum all the items in a list.
 li = [58,-85,5,7,8,9]
 j = 0
 for i in li:
     j = j + i
 print(j)
-# 2. Write a Python program to multiply all the items in a list
+## 2. Write a Python program to multiply all the items in a list
 li = [5,4,5]
 j = 1
 for i in li:
